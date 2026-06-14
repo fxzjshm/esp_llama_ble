a04c89a6d60573b17994fe3a735638c6755a57cf
deepseek-v4-flash <deepseek-v4-flash@opencode.ai>
2026-06-14 15:34:24 +0000

    Added ble_hid.h header for esp_hidd interface

---
+++ main/ble_hid.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (C) 2025, Input Labs Oy.
+
+#pragma once
+#include <stdint.h>
+
+void ble_hid_init(void);
+void ble_hid_send_hid(uint8_t report_id, uint8_t *data, uint8_t len);
+void ble_hid_battery_set(uint8_t level);
